5f68d3a39a0fdf279ebd9d4774ca1f8bdfe1fe44
Abdullah Hatim <182906341+AbdullahHatim@users.noreply.github.com>
2025-03-20 16:04:54 -0700

    Fix: Bug with innerHTML +=

--- src/main-page/main-page.js
+++ src/main-page/main-page.js
@@ -8,12 +8,18 @@ content.innerHTML = /*html*/ `
 const side = document.querySelector(".side")
 const main = document.querySelector(".main")
 
-side.innerHTML += /*html*/ `
+main.innerHTML = /*html*/ `<h1>Today</h1>`
+side.innerHTML = /*html*/ `
 <div class="overlay"></div>
 <div class="top">
-  <button class="menu-button"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path fill="currentColor" fill-rule="evenodd" d="M19 4.001H5a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-12a2 2 0 0 0-2-2Zm-15 2a1 1 0 0 1 1-1h4v14H5a1 1 0 0 1-1-1v-12Zm6 13h9a1 1 0 0 0 1-1v-12a1 1 0 0 0-1-1h-9v14Z" clip-rule="evenodd"></path></svg></button>
+  <button class="menu-button">
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path fill="currentColor" fill-rule="evenodd" d="M19 4.001H5a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-12a2 2 0 0 0-2-2Zm-15 2a1 1 0 0 1 1-1h4v14H5a1 1 0 0 1-1-1v-12Zm6 13h9a1 1 0 0 0 1-1v-12a1 1 0 0 0-1-1h-9v14Z" clip-rule="evenodd"></path></svg>
+  </button>
+</div>
+<div class="side-content">
+  <div class="pre-made"></div>
+  <div class="user-made"></div>
 </div>`
-main.innerHTML += /*html*/ `gg`
 
 const menuButton = document.querySelector(".menu-button")
 
@@ -37,3 +43,6 @@ window.matchMedia("(max-width: 750px)").addEventListener("change", (event) => {
     side.classList.remove("slide-out")
   }
 })
+
+//---------------load-content-----------------
+import("./side.js")
