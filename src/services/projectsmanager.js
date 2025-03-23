52d5bd4d204c1c8982181e81af6fdc5f1f8b60c4
Abdullah Hatim <182906341+AbdullahHatim@users.noreply.github.com>
2025-03-23 15:22:59 -0700

    Fix: Seperate Pre Defined Projects

--- src/services/projectsmanager.js
+++ src/services/projectsmanager.js
@@ -1,21 +1,30 @@
 import storage from "./storage"
 import { ProjectList } from "../components/project/projectlist"
 import { getTodosDueToday } from "@/components/utils/gettodosduetoday"
+import { Project } from "@/components/project/project"
+
+const GENERAL_ID = "8adb2c18cafb0d0ad1a0113a080af51e"
+const TODAY_ID = "7925cbea7729ed237b37d5de3dc96218"
+const DEFAULT_TODOLIST_ID = "7a109073db7959600fcfa3414e7c85e6"
 
 export const ProjectManager = (() => {
-  const storageKey = "projectlist"
-  const projectListJSON = storage.get(storageKey)
+  const mainKey = "projectlist"
+  const preDefinedKey = "predefined"
+  const projectListJSON = storage.get(mainKey)
+  const preDefinedJSON = storage.get(preDefinedKey)
   const projectList = new ProjectList()
-  const GENERAL_ID = "8adb2c18cafb0d0ad1a0113a080af51e"
-  const TODAY_ID = "7925cbea7729ed237b37d5de3dc96218"
-  const DEFAULT_TODOLIST_ID = "7a109073db7959600fcfa3414e7c85e6"
+  const predefinedProjectList = new ProjectList()
 
   if (projectListJSON) {
     projectList.parse(projectListJSON)
   }
+  if (preDefinedJSON) {
+    predefinedProjectList.parse(projectListJSON)
+  }
 
   const updateStorage = () => {
-    storage.set(storageKey, projectList.toString())
+    storage.set(mainKey, projectList.toString())
+    storage.set(preDefinedKey, predefinedProjectList.toString())
   }
 
   const addProject = (title) => {
@@ -39,25 +48,41 @@ export const ProjectManager = (() => {
   }
 
   function init() {
-    if (!getProject(GENERAL_ID)) {
-      addProject(GENERAL_ID)
+    if (!predefinedProjectList.getItem(GENERAL_ID)) {
+      predefinedProjectList.addItem(GENERAL_ID).getItem().addItem(DEFAULT_TODOLIST_ID)
     }
 
-    if (!getProject(TODAY_ID)) {
-      addProject(TODAY_ID)
+    if (!predefinedProjectList.getItem(TODAY_ID)) {
+      predefinedProjectList.addItem(TODAY_ID).getItem().addItem(DEFAULT_TODOLIST_ID)
     }
-    const today = getProject(TODAY_ID)
+    updateStorage()
+  }
+
+  init()
+
+  function getGeneralProject() {
+    return predefinedProjectList.getItem(GENERAL_ID)
+  }
+  function getTodayProject() {
+    const today = predefinedProjectList.getItem(TODAY_ID)
     today.update = () => {
       today.getItem().clear()
       for (const todo of getTodosDueToday(projectList)) {
         today.getItem().addItem(todo)
       }
+      for (const todo of getTodosDueToday(predefinedProjectList)) {
+        today.getItem().addItem(todo)
+      }
     }
-
-    console.log(projectList)
+    today.update()
+    return today
   }
-
-  init()
+  function isGeneralProject(project = new Project()) {
+    return project === predefinedProjectList.getItem(GENERAL_ID)
+  }
+  function isTodayProject(project = new Project()) {
+    return project === predefinedProjectList.getItem(TODAY_ID)
+  }
 
   return Object.freeze({
     updateStorage,
@@ -65,6 +90,10 @@ export const ProjectManager = (() => {
     getProject,
     removeProject,
     reset,
+    getGeneralProject,
+    isGeneralProject,
+    getTodayProject,
+    isTodayProject,
     get projectList() {
       return projectList
     },
