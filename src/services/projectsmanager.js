460f928dfc500b59b80a43ca83981643310d4f07
Abdullah Hatim <182906341+AbdullahHatim@users.noreply.github.com>
2025-03-23 14:16:46 -0700

    Fix: Decouple part 1

--- src/services/projectsmanager.js
+++ src/services/projectsmanager.js
@@ -1,10 +1,14 @@
 import storage from "./storage"
 import { ProjectList } from "../components/project/projectlist"
+import { getTodosDueToday } from "@/components/utils/gettodosduetoday"
 
 export const ProjectManager = (() => {
   const storageKey = "projectlist"
   const projectListJSON = storage.get(storageKey)
   const projectList = new ProjectList()
+  const GENERAL_ID = "8adb2c18cafb0d0ad1a0113a080af51e"
+  const TODAY_ID = "7925cbea7729ed237b37d5de3dc96218"
+  const DEFAULT_TODOLIST_ID = "7a109073db7959600fcfa3414e7c85e6"
 
   if (projectListJSON) {
     projectList.parse(projectListJSON)
@@ -16,10 +20,12 @@ export const ProjectManager = (() => {
 
   const addProject = (title) => {
     projectList.addItem(title)
+    getProject().addItem(DEFAULT_TODOLIST_ID)
     updateStorage()
   }
-  const getProject = (title) => {
-    return projectList.getItem(title)
+
+  const getProject = (value) => {
+    return projectList.getItem(value)
   }
   const removeProject = (title) => {
     const project = projectList.removeItem(title)
@@ -27,16 +33,38 @@ export const ProjectManager = (() => {
     return project
   }
 
-  const resetAllProjects = () => {
+  const reset = () => {
     projectList.clear()
     updateStorage()
   }
+
+  function init() {
+    if (!getProject(GENERAL_ID)) {
+      addProject(GENERAL_ID)
+    }
+
+    if (!getProject(TODAY_ID)) {
+      addProject(TODAY_ID)
+    }
+    const today = getProject(TODAY_ID)
+    today.update = () => {
+      today.getItem().clear()
+      for (const todo of getTodosDueToday(projectList)) {
+        today.getItem().addItem(todo)
+      }
+    }
+
+    console.log(projectList)
+  }
+
+  init()
+
   return Object.freeze({
     updateStorage,
     addProject,
     getProject,
     removeProject,
-    resetAllProjects,
+    reset,
     get projectList() {
       return projectList
     },
