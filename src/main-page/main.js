31f4508e76ddea43e8c316cfd8d079b019cb4f87
Abdullah Hatim <182906341+AbdullahHatim@users.noreply.github.com>
2025-03-26 00:01:32 -0700

    Feat: Add <p> inside addListButton

--- src/main-page/main.js
+++ src/main-page/main.js
@@ -134,6 +134,39 @@ function getContent() {
 
       block.appendChild(addTaskButton)
     }
+    function renderAddListButton() {
+      const addListButton = document.createElement("button")
+      addListButton.className = "menu-item add-list"
+      addListButton.innerHTML = `<p>Add List</p>`
+      addListButton.addEventListener("click", () => {
+        const prompt = modal.prompt("Enter List Name")
+        prompt.addEventListener("click", () => {
+          project.addItem(prompt.getInput())
+          ProjectManager.updateStorage()
+          renderProject()
+        })
+      })
+      content.append(addListButton)
+    }
+    function renderRemoveButton(todolist, button) {
+      const removeIcon = `<svg xmlns="http://www.w3.org/2000/svg"><g fill="none" fill-rule="evenodd"><path d="M0 0h24v24H0z"></path><rect width="14" height="1" x="5" y="6" fill="currentColor" rx="0.5"></rect><path fill="currentColor" d="M10 9h1v8h-1V9zm3 0h1v8h-1V9z"></path><path stroke="currentColor" d="M17.5 6.5h-11V18A1.5 1.5 0 0 0 8 19.5h8a1.5 1.5 0 0 0 1.5-1.5V6.5zm-9 0h7V5A1.5 1.5 0 0 0 14 3.5h-4A1.5 1.5 0 0 0 8.5 5v1.5z"></path></g></svg>`
+      const removeButton = document.createElement("button")
+      removeButton.classList.add("remove-button")
+      removeButton.innerHTML = removeIcon
+      button.appendChild(removeButton)
+      removeButton.addEventListener("click", (e) => {
+        e.stopPropagation()
+        function remove() {
+          project.list = project.list.filter((t) => t !== todolist)
+          ProjectManager.updateStorage()
+          renderProject()
+        }
+        const okButton = modal.confirm(`Delete List "${todolist.title}"?`)
+        okButton.addEventListener("click", () => {
+          remove()
+        })
+      })
+    }
     const defaultBlockDiv = (function getDefaultBlock() {
       const block = document.createElement("div")
       block.className = "block default-block"
@@ -179,18 +212,6 @@ function getContent() {
       return block
     })()
     content.append(defaultBlockDiv)
-    //testing
-    // project.addItem("New TodoList test 1").getItem().addItem("Todo 1").addItem("Todo 2")
-    // project.addItem("New TodoList test 2").getItem().addItem("Todo 1").addItem("Todo 2").addItem("Todo 3")
-    // project
-    //   .addItem("New TodoList test 3")
-    //   .getItem()
-    //   .addItem("Todo 1")
-    //   .addItem("Todo 2")
-    //   .addItem("Todo 3")
-    //   .addItem("Todo 4")
-    // ProjectManager.updateStorage()
-    //end: testing
 
     function getBlock(todoList) {
       const block = document.createElement("details")
@@ -224,6 +245,7 @@ function getContent() {
 
       renderTodos(todoList, block)
       renderAddTodoButton(todoList, block)
+      renderRemoveButton(todoList, block.querySelector("summary"))
       return block
     }
     for (const todoList of project.list) {
@@ -232,6 +254,7 @@ function getContent() {
 
       content.append(getBlock(todoList))
     }
+    renderAddListButton()
   }
 
   function showDefaultProject() {
